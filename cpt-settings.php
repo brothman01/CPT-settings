e9c9a53d9428e6f5a2d887183bf394bd2f0f6bc6
Ben Rothman <Ben@BenRothman.org>
2024-12-07 13:09:14 -0500

    Update cpt-settings.php

--- cpt-settings.php
+++ cpt-settings.php
@@ -57,31 +57,43 @@ class CPT_Settings {
             return;
         }
 
-        if ( isset( $_POST['events'] ) && check_admin_referer( 'CPT_Settings_events_nonce', 'CPT_Settings_events_nonce_field' ) ) {
-            $events = array();
-            foreach ( $_POST['events'] as $event ) {
+        if ( isset( $_POST['types'] ) && check_admin_referer( 'CPT_Settings_types_nonce', 'CPT_Settings_types_nonce_field' ) ) {
+            $types = array();
+            foreach ( $_POST['types'] as $cpt ) {
                 // Sanitize each field individually.
-                $events[] = array(
-                    'single_name'  => sanitize_text_field( $event['single_name'] ),
-                    'plural_name'  => sanitize_text_field( $event['plural_name'] ),
-                    'key'          => sanitize_text_field( $event['key'] ),
-                    'description'  => sanitize_text_field( $event['description'] ),
-                    'public'       => sanitize_text_field( $event['public'] ),
-                    'supports'     => sanitize_text_field( $event['supports'] ),
-                    'taxonomies'   => sanitize_text_field( $event['taxonomies'] ),
-                    'icon'         => sanitize_text_field( $event['icon'] ),
+                $cpts[] = array(
+                    'single_name'  => sanitize_text_field( $cpt['single_name'] ),
+                    'plural_name'  => sanitize_text_field( $cpt['plural_name'] ),
+                    'key'          => sanitize_text_field( $cpt['key'] ),
+                    'description'  => sanitize_text_field( $cpt['description'] ),
+                    'public'       => sanitize_text_field( $cpt['public'] ),
+                    'supports' => array('title', 'editor', 'thumbnail'),
+                    'taxonomies'   => sanitize_text_field( $cpt['taxonomies'] ),
+                    'icon'         => sanitize_text_field( $cpt['icon'] ),
+                    'rewrite' => array('slug' => 'jam'), // Optional, for custom URL structure
+                    'hierarchical' => false, // This ensures that this post type is not hierarchical like pages
+                    'show_in_menu' => true, // Ensure it's accessible in the WordPress admin menu
+                    'show_in_nav_menus' => true, // Allow it in the navigation menus
+                    'exclude_from_search' => false, // Allow it to show up in searches
+                    'publicly_queryable' => true, // Important for single view to work correctly
+                    'public' => true,
+                    'has_archive' => true,
+                    'show_ui' => true,
+                    'show_in_rest' => true, // Enable Gutenberg editor support if needed
                 );
             }
-            update_option( 'custom_post_types', $events );
+            update_option( 'custom_post_types', $cpts );
         }
 
-        $events = get_option( 'custom_post_types', array() );
+        $types = get_option( 'custom_post_types', array() );
         ?>
         <div class="wrap">
             <h1><?php esc_html_e( 'Custom Post Type Settings', 'cpt-maker' ); ?></h1>
             <h2>Add, Create or Remove CPTs (Custom Post Types) below! Don't forget to save, happy WordPressing!</h2>
             <form method="post" id="cpt-form" action="">
-                <?php wp_nonce_field( 'CPT_Settings_events_nonce', 'CPT_Settings_events_nonce_field' ); ?>
+                <?php wp_nonce_field( 'CPT_Settings_types_nonce', 'CPT_Settings_types_nonce_field' ); ?>
+
+
                 <table class="wp-list-table widefat fixed striped" style="padding-bottom: 15px; margin-bottom: 15px;">
                     <thead>
                         <tr>
@@ -96,35 +108,35 @@ class CPT_Settings {
                             <th><?php esc_html_e( 'Actions', 'cpt-maker' ); ?></th>
                         </tr>
                     </thead>
-                    <tbody id="events-container">
-                        <?php foreach ( $events as $index => $event ) : ?>
-                            <tr class="event">
+                    <tbody id="types-container">
+                        <?php foreach ( $types as $index => $type ) : ?>
+                            <tr class="type">
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][single_name]" value="<?php echo esc_attr( $event['single_name'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Name (Singular)', 'cpt-maker' ); ?>" style="width: 100%;" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][single_name]" value="<?php echo esc_attr( $type['single_name'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Name (Singular)', 'cpt-maker' ); ?>" style="width: 100%;" />
                                 </td>
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][plural_name]" value="<?php echo esc_attr( $event['plural_name'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Name (Plural)', 'cpt-maker' ); ?>" style="width: 100%;" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][plural_name]" value="<?php echo esc_attr( $type['plural_name'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Name (Plural)', 'cpt-maker' ); ?>" style="width: 100%;" />
                                 </td>
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][key]" value="<?php echo esc_attr( $event['key'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Key', 'cpt-maker' ); ?>" style="width: 100%;" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][key]" value="<?php echo esc_attr( $type['key'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Key', 'cpt-maker' ); ?>" style="width: 100%;" />
                                 </td>
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][description]" value="<?php echo esc_attr( $event['description'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Description', 'cpt-maker' ); ?>" style="width: 100%;" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][description]" value="<?php echo esc_attr( $type['description'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'Description', 'cpt-maker' ); ?>" style="width: 100%;" />
                                 </td>
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][public]" value="<?php echo esc_attr( $event['public'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'true/false', 'cpt-maker' ); ?>" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][public]" value="<?php echo esc_attr( $type['public'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'true/false', 'cpt-maker' ); ?>" />
                                 </td>
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][supports]" value="<?php echo esc_attr( $event['supports'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'ie: title, editor, thumbnail', 'cpt-maker' ); ?>" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][supports]" value="<?php echo esc_attr( $type['supports'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'ie: title, editor, thumbnail', 'cpt-maker' ); ?>" />
                                 </td>
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][taxonomies]" value="<?php echo esc_attr( $event['taxonomies'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'ie: category, post_tag', 'cpt-maker' ); ?>" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][taxonomies]" value="<?php echo esc_attr( $type['taxonomies'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'ie: category, post_tag', 'cpt-maker' ); ?>" />
                                 </td>
                                 <td>
-                                    <input type="text" name="events[<?php echo esc_attr( $index ); ?>][icon]" value="<?php echo esc_attr( $event['icon'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'ie: dashicons-car', 'cpt-maker' ); ?>" />
+                                    <input type="text" name="types[<?php echo esc_attr( $index ); ?>][icon]" value="<?php echo esc_attr( $type['icon'] ?? '' ); ?>" placeholder="<?php esc_attr_e( 'ie: dashicons-car', 'cpt-maker' ); ?>" />
                                 </td>
                                 <td>
-                                    <button type="button" class="remove-event button"><?php esc_html_e( 'Remove', 'cpt-maker' ); ?></button>
+                                    <button type="button" class="remove-type button"><?php esc_html_e( 'Remove', 'cpt-maker' ); ?></button>
                                 </td>
                             </tr>
                         <?php endforeach; ?>
@@ -141,43 +153,43 @@ class CPT_Settings {
      * Register multiple custom post types.
      */
     public function register_multiple_cpts() {
-        $events = get_option( 'custom_post_types', array() );
-        foreach ( $events as $event ) {
+        $types = get_option( 'custom_post_types', array() );
+        foreach ( $types as $type ) {
             $labels = array(
-                'name'               => $event['plural_name'],
-                'singular_name'      => $event['single_name'],
-                'add_new'            => 'Add ' . $event['single_name'],
-                'add_new_item'       => 'Add New ' . $event['single_name'],
-                'edit_item'          => 'Edit ' . $event['single_name'],
-                'new_item'           => 'New ' . $event['single_name'],
-                'view_item'          => 'View ' . $event['single_name'],
-                'search_items'       => 'Search ' . $event['plural_name'],
-                'not_found'          => 'No ' . $event['plural_name'] . ' found',
-                'not_found_in_trash' => 'No ' . $event['plural_name'] . ' found in Trash',
-                'all_items'          => 'All ' . $event['plural_name'],
-                'archives'           => $event['plural_name'] . ' Archives',
-                'attributes'         => $event['single_name'] . ' Attributes',
-                'insert_into_item'   => 'Insert into ' . $event['single_name'],
-                'uploaded_to_this_item' => 'Uploaded to this ' . $event['single_name'],
+                'name'               => $type['plural_name'],
+                'singular_name'      => $type['single_name'],
+                'add_new'            => 'Add ' . $type['single_name'],
+                'add_new_item'       => 'Add New ' . $type['single_name'],
+                'edit_item'          => 'Edit ' . $type['single_name'],
+                'new_item'           => 'New ' . $type['single_name'],
+                'view_item'          => 'View ' . $type['single_name'],
+                'search_items'       => 'Search ' . $type['plural_name'],
+                'not_found'          => 'No ' . $type['plural_name'] . ' found',
+                'not_found_in_trash' => 'No ' . $type['plural_name'] . ' found in Trash',
+                'all_items'          => 'All ' . $type['plural_name'],
+                'archives'           => $type['plural_name'] . ' Archives',
+                'attributes'         => $type['single_name'] . ' Attributes',
+                'insert_into_item'   => 'Insert into ' . $type['single_name'],
+                'uploaded_to_this_item' => 'Uploaded to this ' . $type['single_name'],
                 'featured_image'     => 'Featured Image',
                 'set_featured_image' => 'Set featured image',
                 'remove_featured_image' => 'Remove featured image',
                 'use_featured_image' => 'Use as featured image',
             );
-
+    
             $args = array(
                 'labels'            => $labels,
-                'public'            => ( 'true' === $event['public'] ),
-                'supports'          => explode( ',', $event['supports'] ),
-                'taxonomies'        => explode( ',', $event['taxonomies'] ),
+                'public'            => ! empty( $type['public'] ) ? ( 'true' === $type['public'] ) : true,
+                'supports'          => ! empty( $type['supports'] ) ? explode( ',', $type['supports'] ) : array( 'title', 'editor' ),
+                'taxonomies'        => ! empty( $type['taxonomies'] ) ? explode( ',', $type['taxonomies'] ) : array(),
                 'show_in_rest'      => true,
                 'has_archive'       => true,
-                'rewrite'           => array( 'slug' => $event['key'] ),
+                'rewrite'           => array( 'slug' => $type['key'] ),
                 'show_in_menu'      => true,
-                'menu_icon'         => $event['icon'],
+                'menu_icon'         => ! empty( $type['icon'] ) ? $type['icon'] : 'dashicons-admin-post',
             );
-
-            register_post_type( $event['key'], $args );
+    
+            register_post_type( $type['key'], $args );
         }
     }
 
